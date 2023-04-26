3afd53c60b5774aa5a6d1dd01d8890258fce88cc
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-04-26 10:49:29 -0700

    ran betty and compiled

--- is_path.c
+++ is_path.c
@@ -10,7 +10,7 @@
 int is_path(char *input)
 {
 	int is_path, i;
-	
+
 	is_path = 0;
 	for (i = 0; input[i] != '\0'; i++)
 	{
--- split_string.c
+++ split_string.c
@@ -5,6 +5,7 @@
 /**
  * split_string - splits a string into an array of each word
  * @str: string
+ * @separator: string separator
  *
  * Return: array of strings
  */
@@ -14,12 +15,12 @@ char **split_string(char *str, char *separator)
 	char *token, **words;
 	int i;
 
-	words = malloc(100 * sizeof(char*));
+	words = malloc(100 * sizeof(char *));
 	if (words == NULL)
 	{
 		exit(1);
 	}
-	
+
 	token = strtok(str, separator);
 	i = 0;
 	while (token != NULL)
